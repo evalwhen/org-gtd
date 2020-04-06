1f26676a3cfdacc6102428008bc35c613351cabc
melp <huzi.master@outlook.com>
2020-04-06 15:12:41 +0800

    add: ido.

--- packages.el
+++ packages.el
@@ -582,12 +582,11 @@ so change the default 'F' binding in the agenda to allow both"
   ;; Targets include this file and any file contributing to the agenda - up to 9 levels deep
   (setq org-refile-targets (quote ((nil :maxlevel . 9)
                                    (org-agenda-files :maxlevel . 9))))
-
   ;; Use full outline paths for refile targets - we file directly with IDO
   (setq org-refile-use-outline-path t)
 
   ;; ;; Targets complete directly with IDO
-  ;; (setq org-outline-path-complete-in-steps nil)
+  (setq org-outline-path-complete-in-steps nil)
 
   ;; Allow refile to create parent tasks with confirmation
   (setq org-refile-allow-creating-parent-nodes (quote confirm))
